99ad5239ec36a70eebda8669c0c1571a21a9a335
liuqixuan <qxsoftware@163.com>
2020-12-03 10:09:09 +0800

    fix name bg

--- cvutils/__init__.py
+++ cvutils/__init__.py
@@ -4,12 +4,12 @@ Description:
 FilePath: /cvutils/cvutils/__init__.py
 Author: qxsoftware@163.com
 Date: 2020-10-23 11:23:14
-LastEditTime: 2020-12-03 09:55:32
+LastEditTime: 2020-12-03 10:04:31
 Refer to: https://github.com/QixuanAI
 '''
 
 from imgetter import *
-from camera-recorder import *
+from camera_recorder import *
 from gen_samples import *
 from graffiti import *
 from schedule import *
